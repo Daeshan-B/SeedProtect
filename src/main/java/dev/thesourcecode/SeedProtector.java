bf2b8937caad9f4700d6c92c89d2400a6a43af7f
Daeshan <daeshan@dveloped.net>
2023-12-03 16:12:19 -0500

    Maven -> Gradle

--- src/main/java/dev/thesourcecode/SeedProtector.java
+++ src/main/java/dev/thesourcecode/SeedProtector.java
@@ -1,5 +1,6 @@
 package dev.thesourcecode;
 
+
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -7,6 +8,7 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.time.Instant;
 import java.util.IdentityHashMap;
 import java.util.Map;
+
 
 public class SeedProtector extends JavaPlugin {
     private Map<Player, Instant> cropMessage = new IdentityHashMap<>();
